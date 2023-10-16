86bd8c3c49fa55e89f33301efecf99290da6963c
Rikko <bulletbill51@yahoo.com>
2023-10-16 02:54:24 -0700

    Added song functionality playSong function executes upon input form submission nextSong function executes upon end of audio event listener subContainer visibility set to none until input form submission

--- javs.js
+++ javs.js
@@ -1,8 +1,27 @@
+const songs = [
+    {
+        title: "Night in Kyoto",
+        artist: "Avbe",
+        song: "Songs/Kyoto.mp3"
+    },
+    {
+        title: "Moonshine",
+        artist: "Prigida",
+        song: "Songs/Moonshine.mp3"
+    }
+]
+
+
+
 let promptInput = document.querySelector("#promptInput");
 let promptSubmit = document.querySelector("#promptButton");
 let promptWindow = document.querySelector("#prompt");
 let currentTask = document.querySelector("#task");
 let currentTime = document.querySelector("#time");
+let mainContainer = document.querySelector("#mainContainer");
+let songAudio = document.querySelector("#songAudio");
+let songTitle = document.querySelector("#songTitle");
+let songArtist = document.querySelector("#songArtist");
 
 function updateTime() {
     let Time = new Date();
@@ -14,9 +33,34 @@ function updateTime() {
    
     currentTime.textContent = displayTime;
 }
+
+let currentSongIndex = 0;
+currentSongIndex = Math.floor(Math.random()*songs.length);
+console.log(currentSongIndex);
+
+function playSong () {
+    let currentSong = songs[currentSongIndex];
+    console.log(currentSong);
+    songAudio.src = currentSong.song;
+    songTitle.textContent = currentSong.title;
+    songArtist.textContent = currentSong.artist;
+    songAudio.play();
+
+}
 promptSubmit.addEventListener("click", () => {
     currentTask.textContent = "Task:" + " " + promptInput.value;
-    promptWindow.style.display="none"
+    promptWindow.style.display= "none";
     updateTime();
+    playSong();
+    mainContainer.style.display = "flex";
     });   
 setInterval(updateTime, 1000);
+
+
+
+function playNextSong() {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+    playSong();
+}
+
+songAudio.addEventListener('ended', playNextSong);
